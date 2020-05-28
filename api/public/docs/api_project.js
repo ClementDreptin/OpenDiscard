cac769b8dbb60e65145b00dccfba8fe6ae672443
Clement <cdreptin@gmail.com>
2020-05-28 15:47:34 +0200

    Documentation for update text channel route

--- api/public/docs/api_project.js
+++ api/public/docs/api_project.js
@@ -7,7 +7,7 @@ define({
   "defaultVersion": "0.0.0",
   "generator": {
     "name": "apidoc",
-    "time": "2020-05-28T12:34:56.064Z",
+    "time": "2020-05-28T13:45:44.396Z",
     "url": "http://apidocjs.com",
     "version": "0.20.1"
   }
