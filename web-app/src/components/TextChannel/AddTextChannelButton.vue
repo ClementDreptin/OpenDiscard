7a51722d06e123d56d8c0003512a727eb8383eeb
Clement <cdreptin@gmail.com>
2020-06-11 17:13:58 +0200

    Create text channel feature is functional

---
+++ web-app/src/components/TextChannel/AddTextChannelButton.vue
@@ -0,0 +1,86 @@
+<template>
+    <div>
+        <div v-show="showModal" class="modal is-active">
+            <div class="modal-background"></div>
+            <div class="modal-card">
+                <form @submit.prevent="">
+                    <header class="modal-card-head">
+                        <p class="modal-card-title">Create a Text Channel</p>
+                        <button @click="showModal = false" class="delete"></button>
+                    </header>
+                    <section class="modal-card-body">
+                        <div class="field">
+                            <p class="control">
+                                <input @keypress="fail = null" class="input" type="text" v-model="textChannelName" placeholder="Name">
+                            </p>
+                        </div>
+                        <div class="container">
+                            <article class="message is-danger" v-show="fail">
+                                <div class="message-header">
+                                    <p>Error</p>
+                                </div>
+                                <div class="message-body">
+                                    {{ fail }}
+                                </div>
+                            </article>
+                        </div>
+                    </section>
+                    <footer class="modal-card-foot">
+                        <button @click="createTextChannel" class="button is-success">Create</button>
+                        <button @click="showModal = false" class="button">Cancel</button>
+                    </footer>
+                </form>
+            </div>
+        </div>
+        <span>
+            <a @click="showModal = true" class="add-text-channel-button">
+                <span>+</span>
+            </a>
+        </span>
+    </div>
+</template>
+
+<script>
+    export default {
+        name: "AddTextChannelButton",
+        data() {
+            return {
+                showModal: false,
+                textChannelName: "",
+                fail: null
+            }
+        },
+        methods: {
+            createTextChannel() {
+                if (this.textChannelName === "") return this.fail = "You must give the text channel a name!";
+
+                axios.post(`servers/${this.$store.state.currentServer.id}/channels/`, {
+                    name: this.textChannelName
+                }).then(response => {
+                    this.$store.state.currentServer.textChannels.push(response.data.text_channel);
+                    this.$bus.$emit('textChannelAdded');
+                    this.showModal = false;
+                    this.textChannelName = "";
+                }).catch(err => console.log(err.response.data.message));
+            }
+        }
+    }
+</script>
+
+<style scoped>
+    .menu-list a:hover {
+        background-color: transparent;
+        color: #a5a5a5;
+    }
+
+    .add-text-channel-button {
+        color: #a5a5a5;
+        font-size: 1.3rem;
+        text-align: right;
+        padding: 0;
+    }
+
+    .add-text-channel-button > span:hover {
+        color: white;
+    }
+</style>
--- web-app/src/components/TextChannel/TextChannels.vue
+++ web-app/src/components/TextChannel/TextChannels.vue
@@ -1,6 +1,9 @@
 <template>
     <aside class="menu">
         <ul class="menu-list">
+            <li v-if="$store.state.currentServer">
+                <AddTextChannelButton/>
+            </li>
             <li v-for="textChannel in textChannels">
                 <TextChannel :textChannel="textChannel"/>
             </li>
@@ -10,11 +13,13 @@
 
 <script>
     import TextChannel from "./TextChannel";
+    import AddTextChannelButton from "./AddTextChannelButton";
 
     export default {
         name: "TextChannels",
         components: {
-            TextChannel
+            TextChannel,
+            AddTextChannelButton
         },
         data() {
             return {
@@ -28,8 +33,10 @@
                         .then(response => {
                             this.$store.state.currentServer.textChannels = response.data.text_channels;
                             this.textChannels = this.$store.state.currentServer.textChannels;
-                            this.$store.state.currentTextChannel = this.textChannels[0];
-                            this.$bus.$emit('currentTextChannelChanged');
+                            if (this.textChannels.length !== 0) {
+                                this.$store.state.currentTextChannel = this.textChannels[0];
+                                this.$bus.$emit('currentTextChannelChanged');
+                            }
                         }).catch(err => console.log(err.response.data.message));
                 } else {
                     this.textChannels = this.$store.state.currentServer.textChannels;
@@ -39,7 +46,11 @@
         mounted() {
             this.$bus.$on('currentServerChanged', () => {
                 this.getTextChannels();
-            })
+            });
+
+            this.$bus.$on('textChannelAdded', () => {
+                this.getTextChannels();
+            });
         }
     }
 </script>
