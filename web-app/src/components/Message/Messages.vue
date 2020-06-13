d7a26ffb5c3ce68ade148ef9919679da2bf4ff3b
Clement <cdreptin@gmail.com>
2020-06-13 17:40:12 +0200

    Added a header with the current text channel name at the top

--- web-app/src/components/Message/Messages.vue
+++ web-app/src/components/Message/Messages.vue
@@ -1,5 +1,8 @@
 <template>
     <div>
+        <header v-if="$store.state.currentTextChannel">
+            <h6 class="title is-6">{{ $store.state.currentTextChannel.name }}</h6>
+        </header>
         <Message v-for="message in messages" :message="message"/>
     </div>
 </template>
@@ -46,5 +49,15 @@
     div {
         background-color: #36393F;
         margin-bottom: 2em;
+    }
+
+    h6 {
+        color: #a5a5a5;
+        white-space: nowrap;
+        text-overflow: ellipsis;
+        overflow: hidden;
+        padding-bottom: 1.25rem;
+        padding-top: 0.5rem;
+        border-bottom: solid 2px #272727;
     }
 </style>
