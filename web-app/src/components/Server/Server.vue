f0ae95a9e25db8f266c31ba6ff8cc2e2c9e083d4
Clement <cdreptin@gmail.com>
2020-06-15 16:17:55 +0200

    When a server doesn't have an image, a text with the first letter of each word of its name is used as an icon

--- web-app/src/components/Server/Server.vue
+++ web-app/src/components/Server/Server.vue
@@ -1,11 +1,17 @@
 <template>
     <figure class="image">
-        <span v-show="isActive"></span>
+        <span class="active-indicator" v-show="isActive"></span>
         <a @click="serverClick">
             <img v-if="server.image_url"
                  :class="{active: isActive}"
                  :src="`${axios.defaults.baseURL}${server.image_url}`"
                  :alt="`Image of the server ${server.name}`">
+
+            <div v-else class="server-icon" :class="{active: isActive}">
+                <div class="server-icon-text">
+                    {{ getServerIconText() }}
+                </div>
+            </div>
         </a>
     </figure>
 </template>
@@ -30,6 +36,20 @@
                 });
                 this.isActive = !this.isActive;
                 this.$bus.$emit('currentServerChanged');
+            },
+
+            getServerIconText() {
+                let serverName = this.server.name;
+                let iconText = serverName[0].toUpperCase();
+                const separators = [' ', '-', '/', '\\', '_', '|', ',', ';', ':', '&'];
+
+                for (let i = 0; i < serverName.length; i++) {
+                    if (separators.includes(serverName[i]) && !separators.includes(serverName[i+1]) && serverName[i+1] !== undefined) {
+                        iconText += serverName[i + 1].toUpperCase();
+                    }
+                }
+
+                return iconText.substring(0, 4);
             }
         }
     }
@@ -64,11 +84,7 @@
         display: inline-block;
     }
 
-    .active {
-        border-radius: 10px;
-    }
-
-    span {
+    .active-indicator {
         position: absolute;
         display: block;
         width: 8px;
@@ -77,5 +93,36 @@
         margin-left: -15px;
         margin-top: 4px;
         background-color: #dcddde;
+    }
+
+    .server-icon-text {
+        padding-top: 14px;
+    }
+
+    .server-icon:hover {
+        border-radius: 10px;
+        background-color: #7289da;
+        color: white;
+    }
+
+    .server-icon.active {
+        background-color: #7289da;
+        color: white;
+    }
+
+    .server-icon {
+        color: #dcddde;
+        border-radius: 100%;
+        width: 48px;
+        height: 48px;
+        background-color: #36393F;
+        -moz-transition: all .3s;
+        -o-transition: all .3s;
+        -webkit-transition: all .3s;
+        transition: all .3s;
+    }
+
+    .active {
+        border-radius: 10px;
     }
 </style>
