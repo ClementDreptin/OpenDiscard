ba9e24faebba7b452e075a31f6d574e6abede83e
Clement <cdreptin@gmail.com>
2020-06-18 18:18:34 +0200

    Added a remove user button, css not done yet

--- web-app/src/components/Users/Member.vue
+++ web-app/src/components/Users/Member.vue
@@ -1,5 +1,5 @@
 <template>
-    <a>
+    <div>
         <span>
             <figure class="image is-32x32">
                 <img v-if="member.avatar_url" class="is-rounded"
@@ -11,7 +11,10 @@
             </figure>
         </span>
         <span class="member-name">{{ member.username }}</span>
-    </a>
+        <a v-if="$store.state.currentServer.owner_id === $store.state.user.id" class="kick-button">
+            <span>X</span>
+        </a>
+    </div>
 </template>
 
 <script>
@@ -29,7 +32,7 @@
 </script>
 
 <style scoped>
-    a {
+    div {
         display: flex;
         padding: 0.4em 0;
     }
@@ -41,19 +44,29 @@
         margin-right: 0.4em;
     }
 
-    a:hover {
-        background-color: #2F3136;
-    }
-
-    a:hover > span {
+    div:hover > span, .kick-button:hover > span {
         color: white;
     }
 
     .member-name {
+        flex-grow: 1;
         margin-top: 0.6em;
         white-space: nowrap;
         text-overflow: ellipsis;
         min-width: 5em;
         overflow: hidden;
+    }
+
+    .kick-button {
+        text-align: right;
+        padding-right: 0;
+    }
+
+    .kick-button:hover {
+        background-color: transparent;
+    }
+
+    .kick-button > span {
+        margin: 0;
     }
 </style>
