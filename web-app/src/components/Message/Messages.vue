6ed9ce24f2e749f0c3309cf08feb7a74e352729d
Clement <cdreptin@gmail.com>
2020-06-22 20:47:44 +0200

    Scrollbar is now by default at the bottom and when the user scrolls to the top the previous messages are fetched

--- web-app/src/components/Message/Messages.vue
+++ web-app/src/components/Message/Messages.vue
@@ -1,5 +1,5 @@
 <template>
-    <div class="messages-container">
+    <div ref="messages" @scroll="onScroll" class="messages-container">
         <div class="messages-custom">
             <Message v-for="message in messages" :message="message"/>
         </div>
@@ -17,33 +17,73 @@
         },
         data() {
             return {
-                messages: []
+                messages: [],
+                nbMessagesFetched: null,
+                currentPage: 1,
+                lastPage: null
             }
         },
         methods: {
-            getMessages(page = 1, size = 10, order = 'desc', authors = true) {
+            getMessages(page = 1, size = 15, order = 'desc', authors = true) {
                 axios.get(`/channels/${this.$store.state.currentTextChannel.id}/messages?page=${page}&size=${size}&order=${order}&authors=${authors}`)
                     .then(response => {
-                        this.$store.state.currentTextChannel.messages = response.data.messages.reverse();
+                        this.nbMessagesFetched = response.data.messages.length;
+
+                        let aroundPage = response.data.links.last.href.split("page=");
+                        let afterPage = aroundPage[aroundPage.length - 1];
+                        this.lastPage = afterPage.split("&")[0];
+
+                        if (this.messages.length === 0) {
+                            this.$store.state.currentTextChannel.messages = response.data.messages.reverse();
+                        } else {
+                            this.$store.state.currentTextChannel.messages = response.data.messages.reverse().concat(this.$store.state.currentTextChannel.messages);
+                        }
                         this.messages = this.$store.state.currentTextChannel.messages;
+                        this.currentPage++;
                     }).catch(err => errorHandler(err, this));
+            },
+
+            onScroll() {
+                if (this.$refs.messages.scrollTop === 0 && this.currentPage <= this.lastPage) {
+                    this.getMessages(this.currentPage);
+                }
+            },
+
+            resetData() {
+                this.messages = [];
+                this.nbMessagesFetched = null;
+                this.currentPage = 1;
+                this.lastPage = null;
+            }
+        },
+        watch: {
+            messages: function(newVal, oldVal) {
+                this.$nextTick(() => {
+                    if (oldVal.length === 0 && newVal.length !== 0) {
+                        this.$refs.messages.scrollTop = this.$refs.messages.scrollHeight;
+                    } else if (this.nbMessagesFetched && this.$refs.messages.firstChild.childNodes[this.nbMessagesFetched]) {
+                        this.$refs.messages.firstChild.childNodes[this.nbMessagesFetched].scrollIntoView();
+                    }
+                });
             }
         },
         mounted() {
             this.$bus.$on('currentTextChannelChanged', () => {
+                this.resetData();
                 this.getMessages();
             });
 
             this.$bus.$on('currentServerChanged', () => {
-                this.messages = [];
+                this.resetData();
             });
 
             this.$bus.$on('currentTextChannelWasDeleted', () => {
-                this.messages = [];
+                this.resetData();
             });
 
             this.$bus.$on('messageReceived', message => {
                 this.messages.push(JSON.parse(message));
+                this.$refs.messages.firstChild.lastChild.scrollIntoView();
             });
         }
     }
