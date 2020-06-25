c23eb5a9d2430847bfad02e01c1ccb327bc023de
Clement <cdreptin@gmail.com>
2020-06-25 15:36:35 +0200

    Made the window maximize on start

--- app/src/background.js
+++ app/src/background.js
@@ -1,42 +1,45 @@
 'use strict'
 
-import { app, protocol, BrowserWindow } from 'electron'
+import { app, protocol, BrowserWindow } from 'electron';
 import {
     createProtocol,
     /* installVueDevtools */
-} from 'vue-cli-plugin-electron-builder/lib'
-const isDevelopment = process.env.NODE_ENV !== 'production'
+} from 'vue-cli-plugin-electron-builder/lib';
+const isDevelopment = process.env.NODE_ENV !== 'production';
 
 // Keep a global reference of the window object, if you don't, the window will
 // be closed automatically when the JavaScript object is garbage collected.
-let win
+let win;
 
 // Scheme must be registered before the app is ready
-protocol.registerSchemesAsPrivileged([{scheme: 'app', privileges: { secure: true, standard: true } }])
+protocol.registerSchemesAsPrivileged([{scheme: 'app', privileges: { secure: true, standard: true } }]);
 
 function createWindow () {
     // Create the browser window.
-    win = new BrowserWindow({ width: 800, height: 600, webPreferences: {
+    win = new BrowserWindow({
+        webPreferences: {
             // Use pluginOptions.nodeIntegration, leave this alone
             // See nklayman.github.io/vue-cli-plugin-electron-builder/guide/security.html#node-integration for more info
             nodeIntegration: process.env.ELECTRON_NODE_INTEGRATION
-        } })
+        }
+    });
 
     if (process.env.WEBPACK_DEV_SERVER_URL) {
         // Load the url of the dev server if in development mode
-        win.loadURL(process.env.WEBPACK_DEV_SERVER_URL)
-        if (!process.env.IS_TEST) win.webContents.openDevTools()
+        win.loadURL(process.env.WEBPACK_DEV_SERVER_URL);
+        if (!process.env.IS_TEST) win.webContents.openDevTools();
     } else {
-        createProtocol('app')
+        createProtocol('app');
         // Load the index.html when not in development
-        win.loadURL('app://./index.html')
+        win.loadURL('app://./index.html');
     }
 
     win.on('closed', () => {
-        win = null
-    })
+        win = null;
+    });
 
     win.removeMenu();
+    win.maximize();
 }
 
 // Quit when all windows are closed.
@@ -44,7 +47,7 @@ app.on('window-all-closed', () => {
     // On macOS it is common for applications and their menu bar
     // to stay active until the user quits explicitly with Cmd + Q
     if (process.platform !== 'darwin') {
-        app.quit()
+        app.quit();
     }
 })
 
@@ -52,7 +55,7 @@ app.on('activate', () => {
     // On macOS it's common to re-create a window in the app when the
     // dock icon is clicked and there are no other windows open.
     if (win === null) {
-        createWindow()
+        createWindow();
     }
 })
 
@@ -74,20 +77,20 @@ app.on('ready', async () => {
         // }
 
     }
-    createWindow()
-})
+    createWindow();
+});
 
 // Exit cleanly on request from parent process in development mode.
 if (isDevelopment) {
     if (process.platform === 'win32') {
         process.on('message', data => {
             if (data === 'graceful-exit') {
-                app.quit()
+                app.quit();
             }
-        })
+        });
     } else {
         process.on('SIGTERM', () => {
-            app.quit()
-        })
+            app.quit();
+        });
     }
 }
