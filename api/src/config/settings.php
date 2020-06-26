affbdc9eb8e99ed89e8191022591f6b88f673876
Clement <cdreptin@gmail.com>
2020-06-26 22:39:44 +0200

    Added download directory to settings

--- api/src/config/settings.php
+++ api/src/config/settings.php
@@ -5,6 +5,7 @@ return [
         'dbconf' => '../src/config/config.ini',
         'JWT_secret' => parse_ini_file('../src/config/config.ini')['jwtsecret'],
         'determineRouteBeforeAppMiddleware' => true,
-        'upload_dir' => '/var/www/images'
+        'upload_dir' => '/var/www/images',
+        'download_dir' => '/var/www/download'
     ]
 ];
