2d803df1805358ea45d0be5b0723aee740070714
Clement <cdreptin@gmail.com>
2020-06-30 17:29:40 +0200

    Added entities to state

--- mobile-app/app/store/index.js
+++ mobile-app/app/store/index.js
@@ -5,7 +5,10 @@ Vue.use(Vuex);
 
 export default new Vuex.Store({
     state: {
-
+        user: null,
+        servers: [],
+        currentServer: null,
+        currentTextChannel: null
     },
     mutations: {
 
