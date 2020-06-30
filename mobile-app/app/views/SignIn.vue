b5c2779bffd2dd27339a641df1fbfdbc9cc5532d
Clement <cdreptin@gmail.com>
2020-06-30 20:22:11 +0200

    Added goToSignUp function

--- mobile-app/app/views/SignIn.vue
+++ mobile-app/app/views/SignIn.vue
@@ -1,6 +1,6 @@
 <template>
     <Page actionBarHidden="true">
-        <FlexboxLayout>
+        <FlexboxLayout class="sign-form">
             <StackLayout class="form">
                 <Image class="logo" src="res://icon" />
                 <Label class="header" text="OpenDiscard"/>
@@ -15,10 +15,10 @@
                     <StackLayout class="hr-light" />
                 </StackLayout>
 
-                <Button text="Sign In" @tap="signIn" class="btn btn-primary m-t-20" />
+                <Button text="Sign In" @tap="signIn" class="btn" />
             </StackLayout>
 
-            <Label class="login-label sign-up-label" @tap="goToSignUp">
+            <Label class="switch-page-label" @tap="goToSignUp">
                 <FormattedString>
                     <Span text="Don’t have an account? Sign Up!" />
                 </FormattedString>
@@ -29,14 +29,14 @@
 
 <script>
     import Home from "~/views/Home";
+    import SignUp from "~/views/SignUp";
 
     export default {
         name: "SignIn",
         data() {
             return {
                 email: "caden@test.com",
-                password: "1234",
-                fail: null
+                password: "1234"
             }
         },
         methods: {
@@ -55,7 +55,7 @@
             },
 
             goToSignUp() {
-
+                this.$navigateTo(SignUp).catch(err => console.log(err));
             }
         },
         mounted() {
@@ -67,59 +67,5 @@
 </script>
 
 <style scoped lang="scss">
-    .page {
-        align-items: center;
-        flex-direction: column;
-    }
-    .form {
-        margin-left: 30;
-        margin-right: 30;
-        flex-grow: 2;
-        vertical-align: middle;
-    }
 
-    .logo {
-        margin-bottom: 12;
-        height: 90;
-        font-weight: bold;
-    }
-    .header {
-        horizontal-align: center;
-        font-size: 25;
-        font-weight: 600;
-        margin-bottom: 50;
-        text-align: center;
-        color: #53ba82;
-    }
-
-    .input-field {
-        margin-bottom: 25;
-    }
-    .input {
-        font-size: 18;
-        placeholder-color: #A8A8A8;
-    }
-    .input-field .input {
-        font-size: 54;
-    }
-
-    .btn-primary {
-        height: 50;
-        margin: 30 5 15 5;
-        background-color: #D51A1A;
-        border-radius: 5;
-        font-size: 20;
-        font-weight: 600;
-    }
-    .login-label {
-        horizontal-align: center;
-        color: #A8A8A8;
-        font-size: 16;
-    }
-    .sign-up-label {
-        margin-bottom: 20;
-    }
-    .bold {
-        color: #000000;
-    }
 </style>
