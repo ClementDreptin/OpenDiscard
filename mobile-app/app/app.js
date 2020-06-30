30c55491b4a8a78fec1c5c8334ae0a45de7d6148
Clement <cdreptin@gmail.com>
2020-06-30 18:58:26 +0200

    Added base64 functions

--- mobile-app/app/app.js
+++ mobile-app/app/app.js
@@ -2,12 +2,23 @@ import Vue from "nativescript-vue";
 import store from "./store";
 import axios from "axios";
 import App from "./views/App";
+import {decode, encode} from 'base-64';
 
+// Base 64
+if (!global.btoa) {
+    global.btoa = encode;
+}
+
+if (!global.atob) {
+    global.atob = decode;
+}
+
+// Global instance of axios
 global.axios = axios.create({
     baseURL: 'http://51.68.213.80:19180'
 });
 
 new Vue({
     store,
-    render: h => h(App)
+    render: h => h('frame', [h(App)])
 }).$start();
