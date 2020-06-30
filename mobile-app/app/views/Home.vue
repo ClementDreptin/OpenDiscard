52ac8a1a3a2ef7ad50b11f039336a6ade4704713
Clement <cdreptin@gmail.com>
2020-06-30 18:59:40 +0200

    Put the general template in a Home component and replaced template of App with a SignIn instance

---
+++ mobile-app/app/views/Home.vue
@@ -0,0 +1,58 @@
+<template>
+    <Page>
+        <BottomNavigation>
+            <TabStrip>
+                <TabStripItem class="navigation__item">
+                    <Label text="Servers"></Label>
+                    <Image src.decode="font://&#xf022;" class="fas t-36"></Image>
+                </TabStripItem>
+                <TabStripItem class="navigation__item">
+                    <Label text="Channel"></Label>
+                    <Image src.decode="font://&#xf4ad;" class="fas t-36"></Image>
+                </TabStripItem>
+                <TabStripItem class="navigation__item">
+                    <Label text="Members"></Label>
+                    <Image src.decode="font://&#xf0c0;" class="fas t-36"></Image>
+                </TabStripItem>
+            </TabStrip>
+
+            <TabContentItem>
+                <Frame id="servers">
+                    <Servers/>
+                </Frame>
+            </TabContentItem>
+
+            <TabContentItem>
+                <Frame id="messages">
+                    <Messages/>
+                </Frame>
+            </TabContentItem>
+
+            <TabContentItem>
+                <Frame id="members">
+                    <Members/>
+                </Frame>
+            </TabContentItem>
+
+        </BottomNavigation>
+    </Page>
+</template>
+
+<script>
+    import Servers from "./Servers";
+    import Messages from "./Messages";
+    import Members from "./Members";
+
+    export default {
+        name: "Home",
+        components: {
+            Servers,
+            Messages,
+            Members
+        }
+    }
+</script>
+
+<style scoped lang="scss">
+
+</style>
