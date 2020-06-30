f7df7fd4b8cd263d49699eb53010a22fe31ebba2
Clement <cdreptin@gmail.com>
2020-06-30 17:28:12 +0200

    Imported axios and the store

--- mobile-app/app/app.js
+++ mobile-app/app/app.js
@@ -1,7 +1,13 @@
 import Vue from "nativescript-vue";
-
+import store from "./store";
+import axios from "axios";
 import App from "./views/App";
 
+global.axios = axios.create({
+    baseURL: 'http://51.68.213.80:19180'
+});
+
 new Vue({
+    store,
     render: h => h(App)
 }).$start();
