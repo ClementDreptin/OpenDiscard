d56d5c5bb112892279e8f02dd0930beb8021b323
Clement <cdreptin@gmail.com>
2020-06-30 19:00:50 +0200

    Added Server component

---
+++ mobile-app/app/components/Server/Server.vue
@@ -0,0 +1,18 @@
+<template>
+    <FlexboxLayout>
+
+    </FlexboxLayout>
+</template>
+
+<script>
+    export default {
+        name: "Server",
+        props: [
+            'server'
+        ]
+    }
+</script>
+
+<style scoped lang="scss">
+
+</style>
