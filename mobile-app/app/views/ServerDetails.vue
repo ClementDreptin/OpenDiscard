280f37eff5d74665bdc02be969e160ac4c6e6ea5
Clement <cdreptin@gmail.com>
2020-07-01 18:24:08 +0200

    Get servers functional

--- mobile-app/app/views/ServerDetails.vue
+++ mobile-app/app/views/ServerDetails.vue
@@ -2,18 +2,18 @@
     <Page>
         <ActionBar>
             <NavigationButton @tap="$navigateBack()" android.systemIcon="ic_menu_back"/>
-            <Label :text="item.name"></Label>
+            <Label :text="server.name"></Label>
         </ActionBar>
 
         <GridLayout>
-            <Label class="m-10 h3" :text="item.description" verticalAlignment="top"></Label>
+            <Label class="m-10 h3" :text="server.name" verticalAlignment="top"></Label>
         </GridLayout>
     </Page>
 </template>
 
 <script>
     export default {
-        props: ["item"]
+        props: ["server"]
     };
 </script>
 
--- mobile-app/app/views/Servers.vue
+++ mobile-app/app/views/Servers.vue
@@ -4,10 +4,10 @@
             <Label text="Servers"></Label>
         </ActionBar>
 
-        <ListView for="item in items" @itemTap="onItemTap">
+        <ListView for="server in servers" @itemTap="serverClick">
             <v-template>
                 <StackLayout orientation="horizontal">
-                    <Label :text="item.name" textWrap="true"></Label>
+                    <Label :text="server.name" textWrap="true"></Label>
                 </StackLayout>
             </v-template>
         </ListView>
@@ -15,92 +15,40 @@
 </template>
 
 <script>
-    import ItemDetails from "./ItemDetails";
+    import ServerDetails from "./ServerDetails";
+    import errorHandler from "~/modules/Errors";
 
     export default {
         data() {
             return {
-                items: [
-                    {
-                        name: "Item 1",
-                        description: "Description for Item 1"
-                    },
-                    {
-                        name: "Item 2",
-                        description: "Description for Item 2"
-                    },
-                    {
-                        name: "Item 3",
-                        description: "Description for Item 3"
-                    },
-                    {
-                        name: "Item 4",
-                        description: "Description for Item 4"
-                    },
-                    {
-                        name: "Item 5",
-                        description: "Description for Item 5"
-                    },
-                    {
-                        name: "Item 6",
-                        description: "Description for Item 6"
-                    },
-                    {
-                        name: "Item 7",
-                        description: "Description for Item 7"
-                    },
-                    {
-                        name: "Item 8",
-                        description: "Description for Item 8"
-                    },
-                    {
-                        name: "Item 9",
-                        description: "Description for Item 9"
-                    },
-                    {
-                        name: "Item 10",
-                        description: "Description for Item 10"
-                    },
-                    {
-                        name: "Item 11",
-                        description: "Description for Item 11"
-                    },
-                    {
-                        name: "Item 12",
-                        description: "Description for Item 12"
-                    },
-                    {
-                        name: "Item 13",
-                        description: "Description for Item 13"
-                    },
-                    {
-                        name: "Item 14",
-                        description: "Description for Item 14"
-                    },
-                    {
-                        name: "Item 15",
-                        description: "Description for Item 15"
-                    },
-                    {
-                        name: "Item 16",
-                        description: "Description for Item 16"
-                    }
-                ]
+                servers: []
             };
         },
         methods: {
-            onItemTap(args) {
-                this.$navigateTo(ItemDetails, {
+            serverClick(args) {
+                this.$navigateTo(ServerDetails, {
                     frame: 'servers',
-                    props: {item: args.item},
+                    props: {server: args.item},
                     animated: true,
                     transition: {
                         name: "slide",
                         duration: 200,
                         curve: "ease"
                     }
-                });
+                }).catch(err => console.log(err));
+            },
+
+            getServers() {
+                axios.get('/servers/')
+                    .then(response => {
+                        this.$store.state.servers = response.data.servers;
+                        this.servers = this.$store.state.servers;
+                    })
+                    .catch(err => errorHandler(err, this));
             }
+        },
+        mounted() {
+            this.getServers();
         }
     };
 </script>
