1ee9b29db574b2c9b94a9e7fc82018196743349c
Clement <cdreptin@gmail.com>
2020-07-03 19:00:38 +0200

    Button in ServerHeader functional (opens a modal)

--- mobile-app/app/components/Server/ServerHeader.vue
+++ mobile-app/app/components/Server/ServerHeader.vue
@@ -4,6 +4,7 @@
             <Label width="80%"
                    :text="$store.state.currentServer.name"/>
             <Image v-show="$store.state.currentServer.owner_id === $store.state.user.id"
+                   @tap="showSettingsModal"
                    src.decode="font://&#xf013;"
                    stretch="none"
                    color="#dcddde"
@@ -11,6 +12,7 @@
                    class="fas"
                    opacity="0.7"/>
             <Image v-show="$store.state.currentServer.owner_id !== $store.state.user.id"
+                   @tap="showLeaveModal"
                    src.decode="font://&#xf0a8;"
                    stretch="none"
                    color="rgb(201,33,66)"
@@ -23,8 +25,30 @@
 </template>
 
 <script>
+    import ServerSettingsModal from "~/components/Server/ServerSettingsModal";
+    import ConfirmDeleteModal from "~/components/General/ConfirmDeleteModal";
+
     export default {
-        name: "ServerHeader"
+        name: "ServerHeader",
+        methods: {
+            showSettingsModal() {
+                this.$showModal(ServerSettingsModal).catch(err => console.log(err));
+            },
+
+            showLeaveModal() {
+                this.$showModal(ConfirmDeleteModal, {
+                    props: {
+                        element: 'Server',
+                        actionTitle: 'Leave',
+                        deleteFunction: this.leaveServer
+                    }
+                }).catch(err => console.log(err));
+            },
+
+            leaveServer() {
+
+            }
+        }
     }
 </script>
 
