7c495dcd32dd9329b57bccc032218c54eacb5f1b
Clement <cdreptin@gmail.com>
2020-07-05 17:50:08 +0200

    Delete server feature functional

--- mobile-app/app/components/Server/ServerSettingsModal.vue
+++ mobile-app/app/components/Server/ServerSettingsModal.vue
@@ -2,12 +2,14 @@
     <Page>
         <StackLayout class="modal-container">
             <TextField class="modal-input" v-model="serverLocal.name" hint="Server name"/>
+            <Button class="btn-danger" text="Delete this Server" textWrap="false" @tap="showDeleteModal"/>
             <Button class="btn-success" text="Update" @tap="updateServer"/>
         </StackLayout>
     </Page>
 </template>
 
 <script>
+    import ConfirmDeleteModal from "~/components/General/ConfirmDeleteModal";
     import errorHandler from "~/modules/Errors";
 
     export default {
@@ -45,6 +47,40 @@
                     .then(response => {
                         this.$store.state.currentServer.name = response.data.server.name;
                         this.$store.state.currentServer.image_url = response.data.server.image_url;
+                        this.$modal.close();
+                    })
+                    .catch(err => errorHandler(err, this));
+            },
+
+            showDeleteModal() {
+                this.$showModal(ConfirmDeleteModal, {
+                    props: {
+                        element: 'Server',
+                        actionTitle: 'Delete',
+                        deleteFunction: this.deleteServer
+                    }
+                }).catch(err => console.log(err));
+            },
+
+            deleteServer() {
+                global.axios.delete(`/servers/${this.serverLocal.id}`)
+                    .then(response => {
+                        let servers = this.$store.state.servers;
+                        let currentServer = this.$store.state.currentServer;
+                        let currentTextChannel = this.$store.state.currentTextChannel;
+                        let index = servers.findIndex(tc => tc.id === response.data.server.id);
+
+                        servers.splice(index, 1);
+
+                        if (currentServer.id === response.data.server.id) {
+                            this.$store.state.currentServer = null;
+                            global.bus.$emit('currentServerWasDeleted');
+                            if (currentTextChannel && currentTextChannel.server_id === response.data.server.id) {
+                                this.$store.state.currentTextChannel = null;
+                                global.bus.$emit('currentTextChannelWasDeleted');
+                            }
+                        }
+
                         this.$modal.close();
                     })
                     .catch(err => errorHandler(err, this));
