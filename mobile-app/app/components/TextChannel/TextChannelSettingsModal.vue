cd58990500cab839f196088fbf2cf6ed4742f2db
Clement <cdreptin@gmail.com>
2020-07-05 17:43:06 +0200

    Delete text channel feature functional

--- mobile-app/app/components/TextChannel/TextChannelSettingsModal.vue
+++ mobile-app/app/components/TextChannel/TextChannelSettingsModal.vue
@@ -2,12 +2,14 @@
     <Page>
         <StackLayout class="modal-container">
             <TextField class="modal-input" v-model="textChannelLocal.name" hint="Text Channel name"/>
+            <Button class="btn-danger" text="Delete this Text Channel" textWrap="false" @tap="showDeleteModal"/>
             <Button class="btn-success" text="Update" @tap="updateTextChannel"/>
         </StackLayout>
     </Page>
 </template>
 
 <script>
+    import ConfirmDeleteModal from "~/components/General/ConfirmDeleteModal";
     import errorHandler from "~/modules/Errors";
 
     export default {
@@ -44,6 +46,35 @@
 
                         if (currentTextChannel.id === response.data.text_channel.id) {
                             currentTextChannel.name = response.data.text_channel.name;
+                        }
+
+                        this.$modal.close();
+                    })
+                    .catch(err => errorHandler(err, this));
+            },
+
+            showDeleteModal() {
+                this.$showModal(ConfirmDeleteModal, {
+                    props: {
+                        element: 'Text Channel',
+                        actionTitle: 'Delete',
+                        deleteFunction: this.deleteTextChannel
+                    }
+                }).catch(err => console.log(err));
+            },
+
+            deleteTextChannel() {
+                global.axios.delete(`/channels/${this.textChannelLocal.id}`)
+                    .then(response => {
+                        let textChannels = this.$store.state.currentServer.textChannels;
+                        let currentTextChannel = this.$store.state.currentTextChannel;
+                        let index = textChannels.findIndex(tc => tc.id === response.data.text_channel.id);
+
+                        textChannels.splice(index, 1);
+
+                        if (currentTextChannel.id === response.data.text_channel.id) {
+                            this.$store.state.currentTextChannel = null;
+                            global.bus.$emit('currentTextChannelWasDeleted');
                         }
 
                         this.$modal.close();
