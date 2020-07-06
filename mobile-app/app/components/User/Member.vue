b31ce842f8eaabb4ec2995d6d1311625d12d9c9a
Clement <cdreptin@gmail.com>
2020-07-06 15:57:54 +0200

    General layout for the Members view

---
+++ mobile-app/app/components/User/Member.vue
@@ -0,0 +1,54 @@
+<template>
+    <FlexboxLayout>
+        <Image v-if="member.avatar_url"
+               :src="`${axios.defaults.baseURL}${member.avatar_url}`"
+               class="member-avatar"/>
+        <Image v-else
+               src="~/assets/default-profile-picture.png"
+               class="member-avatar"/>
+        <Label :text="member.username" flexGrow="1"/>
+        <Image v-if="$store.state.currentServer && $store.state.currentServer.owner_id === $store.state.user.id && $store.state.currentServer.owner_id !== member.id"
+               src.decode="font://&#xf00d;"
+               stretch="none"
+               fontSize="18"
+               class="fas kick-button"/>
+    </FlexboxLayout>
+</template>
+
+<script>
+    export default {
+        name: "Member",
+        props: [
+            'member'
+        ],
+        data() {
+            return {
+                axios: global.axios
+            }
+        }
+    }
+</script>
+
+<style scoped lang="scss">
+    @import "../../color-variables";
+
+    Label {
+        font-size: 18;
+        color: $white;
+        margin-top: 12;
+        margin-left: 18;
+    }
+
+    .kick-button {
+        color: #c92142;
+        opacity: 0.4;
+        margin-top: 12;
+    }
+
+    .member-avatar {
+        width: 48;
+        height: 48;
+        margin-right: 10;
+        margin-bottom: 10;
+    }
+</style>
--- mobile-app/app/views/Members.vue
+++ mobile-app/app/views/Members.vue
@@ -4,22 +4,93 @@
             <Label text="Members"></Label>
         </ActionBar>
 
-        <GridLayout class="page__content">
-            <Label class="page__content-placeholder" :text="message"></Label>
-        </GridLayout>
+        <StackLayout id="members-container">
+            <ScrollView v-if="$store.state.currentServer">
+                <StackLayout id="members">
+                    <FlexboxLayout class="members-header" justifyContent="space-between">
+                        <Label width="80%"
+                               text="Members"
+                               class="members-header-text"/>
+                        <Image v-if="$store.state.currentServer.owner_id === $store.state.user.id"
+                               @tap="showAddModal"
+                               src.decode="font://&#xf067;"
+                               class="fas members-header-button"
+                               stretch="none"
+                               fontSize="18"
+                               opacity="0.5"/>
+                    </FlexboxLayout>
+                    <Member v-for="member in members" :member="member"/>
+                </StackLayout>
+            </ScrollView>
+        </StackLayout>
     </Page>
 </template>
 
 <script>
+    import Member from "~/components/User/Member";
+    import errorHandler from "~/modules/Errors";
+
     export default {
+        components: {
+            Member
+        },
         data() {
             return {
-                message: "<!-- Members page content goes here -->"
-            };
+                members: []
+            }
+        },
+        methods: {
+            getMembers() {
+                if (!this.$store.state.currentServer.members) {
+                    global.axios.get(`/servers/${this.$store.state.currentServer.id}/users/`)
+                        .then(response => {
+                            this.$store.state.currentServer.members = response.data.members;
+                            this.members = this.$store.state.currentServer.members;
+                        }).catch(err => errorHandler(err, this));
+                } else {
+                    this.members = this.$store.state.currentServer.members;
+                }
+            },
+
+            showAddModal() {
+
+            }
+        },
+        mounted() {
+            global.bus.$on('currentServerChanged', () => {
+                this.getMembers();
+            });
+
+            global.bus.$on('currentServerWasDeleted', () => {
+                this.members = [];
+            });
         }
     }
 </script>
 
 <style lang="scss" scoped>
+    @import "../color-variables";
 
+    #members {
+        padding: 18;
+
+        &-container {
+            background-color: $grey-1;
+        }
+    }
+
+    .members-header {
+        margin-bottom: 18;
+
+        &-text {
+            color: #7a7a7a;
+            font-size: 13.5;
+            letter-spacing: 0.2;
+            text-transform: uppercase;
+        }
+
+        &-button {
+            color: $white;
+        }
+    }
 </style>
