df60bcc5f00ff4f894d90f1092263ca2e681d216
Clement <cdreptin@gmail.com>
2020-07-07 15:30:37 +0200

    Messages view functional

---
+++ mobile-app/app/components/Message/Message.vue
@@ -0,0 +1,58 @@
+<template>
+    <FlexboxLayout>
+        <Image v-if="message.author.avatar_url"
+               :src="`${axios.defaults.baseURL}${message.author.avatar_url}`"
+               class="member-avatar"/>
+        <Image v-else
+               src="~/assets/default-profile-picture.png"
+               class="member-avatar"/>
+        <StackLayout flexGrow="1" class="message-content">
+            <Label :text="message.author.username" class="author-name"/>
+            <Label :text="message.content" textWrap="true"/>
+            <Label :text="message.updated_at" class="date"/>
+        </StackLayout>
+    </FlexboxLayout>
+</template>
+
+<script>
+    export default {
+        name: "Message",
+        props: [
+            'message'
+        ],
+        data() {
+            return {
+                axios: global.axios
+            }
+        }
+    }
+</script>
+
+<style scoped lang="scss">
+    @import "../../color-variables";
+
+    .member-avatar {
+        width: 48;
+        height: 48;
+        margin-right: 10;
+    }
+
+    .author-name {
+        color: white;
+        font-weight: bold;
+    }
+
+    .message-content {
+        color: $white;
+        margin-left: 9;
+        margin-top: 9;
+
+        Label {
+            font-size: 15;
+        }
+
+        .date {
+            color: #72767d;
+        }
+    }
+</style>
--- mobile-app/app/views/Messages.vue
+++ mobile-app/app/views/Messages.vue
@@ -4,22 +4,124 @@
             <Label text="Messages"></Label>
         </ActionBar>
 
-        <GridLayout class="page__content">
-            <Label class="page__content-placeholder" :text="message"></Label>
-        </GridLayout>
+        <StackLayout id="messages-container">
+            <TextChannelHeader v-if="$store.state.currentTextChannel"/>
+            <ActivityIndicator v-if="busy" color="#dcddde" :busy="busy"/>
+            <ScrollView ref="messages" @scroll="onScroll">
+                <StackLayout id="messages">
+                    <Message v-for="(message, index) in messages" :message="message" :ref="'msg' + index"/>
+                </StackLayout>
+            </ScrollView>
+        </StackLayout>
     </Page>
 </template>
 
 <script>
+    import TextChannelHeader from "~/components/TextChannel/TextChannelHeader";
+    import Message from "~/components/Message/Message";
+    import errorHandler from "~/modules/Errors";
+
     export default {
+        components: {
+            Message,
+            TextChannelHeader
+        },
         data() {
             return {
-                message: "<!-- Messages page content goes here -->"
-            };
+                messages: [],
+                nbMessagesFetched: null,
+                currentPage: 1,
+                lastPage: null,
+                busy: false
+            }
+        },
+        methods: {
+            getMessages(page = 1, size = 15, order = 'desc', authors = true) {
+                this.busy = true;
+                global.axios.get(`/channels/${this.$store.state.currentTextChannel.id}/messages?page=${page}&size=${size}&order=${order}&authors=${authors}`)
+                    .then(response => {
+                        this.busy = false;
+
+                        this.nbMessagesFetched = response.data.messages.length;
+
+                        let aroundPage = response.data.links.last.href.split("page=");
+                        let afterPage = aroundPage[aroundPage.length - 1];
+                        this.lastPage = afterPage.split("&")[0];
+
+                        if (this.messages.length === 0) {
+                            this.$store.state.currentTextChannel.messages = response.data.messages.reverse();
+                        } else {
+                            this.$store.state.currentTextChannel.messages = response.data.messages.reverse().concat(this.$store.state.currentTextChannel.messages);
+                        }
+                        this.messages = this.$store.state.currentTextChannel.messages;
+                        this.currentPage++;
+                    }).catch(err => errorHandler(err, this));
+            },
+
+            onScroll(args) {
+                if (args.scrollY === 0 && this.currentPage <= this.lastPage) {
+                    this.getMessages(this.currentPage);
+                }
+            },
+
+            scrollToEnd() {
+                const scrollView = this.$refs.messages.nativeView;
+                scrollView.scrollToVerticalOffset(scrollView.scrollableHeight, true);
+            },
+
+            resetData() {
+                this.messages = [];
+                this.nbMessagesFetched = null;
+                this.currentPage = 1;
+                this.lastPage = null;
+            }
+        },
+        watch: {
+            messages: function(newVal, oldVal) {
+                this.$nextTick(() => {
+                    if (oldVal.length === 0 && newVal.length !== 0) {
+                        setTimeout(() => this.scrollToEnd(), 100);
+                    } else if (this.nbMessagesFetched && this.$refs.messages.firstChild.childNodes[this.nbMessagesFetched - 1]) {
+                        // Apparently refs are arrays with only one element instead of a simple
+                        // object when they're created in a v-for. It makes no sense, I know...
+                        const base = this.$refs.msg0[0].nativeView;
+                        const target = this.$refs.messages.firstChild.childNodes[this.nbMessagesFetched - 1].nativeView;
+                        const scrollView = this.$refs.messages.nativeView;
+                        scrollView.scrollToVerticalOffset(target.getLocationRelativeTo(base).y);
+                    }
+                });
+            }
+        },
+        mounted() {
+            global.bus.$on('currentTextChannelChanged', () => {
+                this.resetData();
+                this.getMessages();
+            });
+
+            global.bus.$on('currentServerChanged', () => {
+                this.resetData();
+            });
+
+            global.bus.$on('currentTextChannelWasDeleted', () => {
+                this.resetData();
+            });
+
+            global.bus.$on('messageReceived', message => {
+                this.messages.push(JSON.parse(message));
+                this.scrollToEnd();
+            });
         }
     }
 </script>
 
 <style lang="scss" scoped>
+    @import "../color-variables";
 
+    #messages-container {
+        background-color: $grey-2;
+    }
+
+    #messages {
+        padding-bottom: 27;
+    }
 </style>
