80756f4ee330932e3b7ae429d3d4db2812b1ba5a
Clement <cdreptin@gmail.com>
2020-07-07 15:31:00 +0200

    Added a text channel header on the messages view

---
+++ mobile-app/app/components/TextChannel/TextChannelHeader.vue
@@ -0,0 +1,26 @@
+<template>
+    <StackLayout id="text-channel-header">
+        <Label :text="$store.state.currentTextChannel.name"/>
+        <StackLayout class="hr"/>
+    </StackLayout>
+</template>
+
+<script>
+    export default {
+        name: "TextChannelHeader"
+    }
+</script>
+
+<style scoped lang="scss">
+    Label {
+        font-size: 18;
+        color: white;
+        text-align: center;
+        margin-bottom: 8;
+    }
+
+    #text-channel-header {
+        padding-top: 8;
+        padding-bottom: 8;
+    }
+</style>
