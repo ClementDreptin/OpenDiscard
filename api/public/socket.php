e6258f41779ae1fb8d66062f0c6cfcb8ec847250
Clement <cdreptin@gmail.com>
2020-07-14 02:44:13 +0200

    Added support for WSS (websocket with TLS)

--- api/public/socket.php
+++ api/public/socket.php
@@ -1,18 +1,28 @@
 <?php
 require '../src/vendor/autoload.php';
 
-use Ratchet\Server\IoServer;
-use Ratchet\Http\HttpServer;
-use Ratchet\WebSocket\WsServer;
 use OpenDiscard\api\socket\Socket;
+use Ratchet\Http\HttpServer;
+use Ratchet\Server\IoServer;
+use Ratchet\WebSocket\WsServer;
+use React\EventLoop\Factory;
+use React\Socket\Server;
+use React\Socket\SecureServer;
 
-$server = IoServer::factory(
-    new HttpServer(
-        new WsServer(
-            new Socket()
-        )
-    ),
-    3000
+$app = new HttpServer(
+    new WsServer(
+        new Socket()
+    )
 );
 
-$server->run();
+$loop = Factory::create();
+
+$secure_websockets = new Server('0.0.0.0:3000', $loop);
+$secure_websockets = new SecureServer($secure_websockets, $loop, [
+    'local_cert' => parse_ini_file('../src/config/config.ini')['cert_path'],
+    'local_pk' => parse_ini_file('../src/config/config.ini')['privkey_path'],
+    'verify_peer' => false
+]);
+
+$secure_websockets_server = new IoServer($app, $secure_websockets, $loop);
+$secure_websockets_server->run();
